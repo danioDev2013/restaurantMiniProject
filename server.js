7663cf72b6db543ba0021b40744306e7e01506e6
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-03-05 19:04:45 -0800

    css work

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 //Express setup
 
-const APP = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Tables and waitlist array.
@@ -15,27 +15,28 @@ let waitlist = [];
 
 //Setsup the Express app to handle data parsing
 
-APP.use(express.urlencoded({ extended: true }));
-APP.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
 
-//Three endpoints for the html files.
-APP.get("/", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
+//Three endpoints for the html files
 
-APP.get("/reservation", (req, res) => res.sendFile(path.join(__dirname, "reservation.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
 
-APP.get("/tables", (req, res) => res.sendFile(path.join(__dirname, "tables.html")));
+app.get("/reservation", (req, res) => res.sendFile(path.join(__dirname, "reservation.html")));
 
-APP.get("/home", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
+app.get("/tables", (req, res) => res.sendFile(path.join(__dirname, "tables.html")));
+
+app.get("/home", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
 
 //Two for getting the two lists.
-APP.get("/api/reservations", (req, res) => res.json(reservations));
+app.get("/api/reservations", (req, res) => res.json(reservations));
 
-APP.get("/api/waitlist", (req, res) => res.json(waitlist));
+app.get("/api/waitlist", (req, res) => res.json(waitlist));
 
 //Post for adding a reservation.
-APP.post("/api/reservations", (req, res) =>
+app.post("/api/reservations", (req, res) =>
 {
     if (reservations.length >= 5)
     {
@@ -49,7 +50,7 @@ APP.post("/api/reservations", (req, res) =>
 });
 
 //Endpoint to clear both lists incase of nuke.
-APP.post("/api/reservations/clear", (req, res) =>
+app.post("/api/reservations/clear", (req, res) =>
 {
     reservations = [];
     waitlist = [];
@@ -58,4 +59,4 @@ APP.post("/api/reservations/clear", (req, res) =>
 });
 
 //Being the server and listen on given port.
-APP.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
